6c74ebce7992efa010e1af13d1ca629b4c2d8263
Iris <leeting110213@gmail.com>
2023-07-07 17:17:53 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ['via.placeholder.com', 'localhost'],
+  },
+  // avoid cors with proxy
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://localhost:3005/:path*', // Proxy to Backend
+  //     },
+  //   ]
+  // },
+}
+
+module.exports = nextConfig
---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import '@/styles/globals.scss'
+import '@/styles/globals.css'
+
+
+import DefaultLayout from '@/components/layout/default-layout'
+
+export default function MyApp({ Component, pageProps }) {
+  
+  useEffect(() => {
+    // 要document物件出現後才能導入 bootstrap的js函式庫
+    import('bootstrap/dist/js/bootstrap')
+  }, [])
+
+  // 使用預設排版檔案
+  // 對應`components/layout/default-layout/index.js`
+  const getLayout =
+    Component.getLayout || ((page) => <DefaultLayout>{page}</DefaultLayout>)
+
+  return getLayout(<Component {...pageProps} />)
+}
