faf6009dc4ea4094044fcbff9f32e6ec2e56c202
Leo Hu <innocence88226@gmail.com>
2023-07-10 15:07:43 +0800

    leo 07/10 0307

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.leo.css'
 import Image from 'next/image'
 import pic1 from '@/public/Leo-image/carousel-1.jpg'
 import pic2 from '@/public/Leo-image/carousel-2.jpg'
@@ -11,7 +11,7 @@ export default function product() {
     <>
       <div
         id="carouselExampleCaptions"
-        className={`carousel slide1 ${carou.box}`}
+        className={`carousel slide1 ${carou.box} d-flex justify-content-center align-items-center`}
       >
         <div className={`carousel-indicators`}>
           <button
@@ -35,11 +35,13 @@ export default function product() {
             aria-label="Slide 3"
           ></button>
         </div>
-        <div className={`carousel-inner`}>
+        <div
+          className={`carousel-inner d-flex  bg-black justify-content-center align-items-center border`}
+        >
           <div className={`carousel-item active`}>
             <Image
               src={pic1}
-              className={`d-block w-100`}
+              className={`d-flex justify-content-center align-items-center`}
               alt="..."
               width={1344}
               height={400}
@@ -54,7 +56,7 @@ export default function product() {
           <div className={`carousel-item`}>
             <Image
               src={pic2}
-              className={`d-block w-100`}
+              className={`d-flex justify-content-center align-items-center`}
               alt="..."
               width={1344}
               height={400}
@@ -69,7 +71,7 @@ export default function product() {
           <div className={`carousel-item`}>
             <Image
               src={pic3}
-              className={`d-block`}
+              className={`d-flex justify-content-center align-items-center`}
               alt="..."
               width={1344}
               height={400}
@@ -107,6 +109,106 @@ export default function product() {
           <span className={`visually-hidden`}>Next</span>
         </button>
       </div>
+      {/* <div
+        id="carouselExampleCaptions"
+        className={`carousel slide1 ${carou.box} d-flex justify-content-center align-items-center`}
+      >
+        <div className={`carousel-indicators`}>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            className={`active`}
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div
+          className={`carousel-inner d-flex  bg-black justify-content-center align-items-center border`}
+        >
+          <div className={`carousel-item active`}>
+            <Image
+              src={pic1}
+              className={`d-flex justify-content-center align-items-center`}
+              alt="..."
+              width={1344}
+              height={400}
+            />
+            <div className={`carousel-caption d-none d-md-block`}>
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className={`carousel-item`}>
+            <Image
+              src={pic2}
+              className={`d-flex justify-content-center align-items-center`}
+              alt="..."
+              width={1344}
+              height={400}
+            />
+            <div className={`carousel-caption d-none d-md-block`}>
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div className={`carousel-item`}>
+            <Image
+              src={pic3}
+              className={`d-flex justify-content-center align-items-center`}
+              alt="..."
+              width={1344}
+              height={400}
+            />
+            <div className={`carousel-caption d-none d-md-block`}>
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className={`carousel-control-prev`}
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span
+            className={`carousel-control-prev-icon`}
+            aria-hidden="true"
+          ></span>
+          <span className={`visually-hidden`}>Previous</span>
+        </button>
+        <button
+          className={`carousel-control-next`}
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span
+            className={`carousel-control-next-icon`}
+            aria-hidden="true"
+          ></span>
+          <span className={`visually-hidden`}>Next</span>
+        </button>
+      </div> */}
     </>
   )
 }
