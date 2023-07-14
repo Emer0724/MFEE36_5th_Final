f24bd3ee4cc3cdd4039f47f990dd73891c472aa9
Leo Hu <innocence88226@gmail.com>
2023-07-14 15:06:28 +0800

    Leo 7.14 1506

--- components/Leo/market_card.js
+++ components/Leo/market_card.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Image from 'next/image'
+import bk from '@/public/Leo-image/心流.webp'
+import car from '@/components/Leo/market_card.module.css'
+import Link from 'next/link'
 
 export default function MarketCard() {
+  const cardRef = useRef(null)
+
+  const handleMouseEnter = () => {
+    cardRef.current.classList.add(car.hovered)
+  }
+  const handleMouseLeave = () => {
+    cardRef.current.classList.remove(car.hovered)
+  }
   return (
-    <div className="market-card-box">
-      <div className="market-card"></div>
-    </div>
+    <>
+      <div
+        className={`col ${car.cardWrapper}`}
+        ref={cardRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className={`${car.card}`}>
+          <div className={`market-card d-flex ${car.no}`}>
+            <Link href="/">
+              <Image
+                src={bk}
+                className={`bk-img`}
+                alt="..."
+                width={170}
+                height={170}
+              />
+            </Link>
+            <div className={`w-100 color-bg-1 h-100 ${car.extra}`}>
+              <p>你好啊!!</p>
+              <p>你好啊!!</p>
+              <p>你好啊!!</p>
+              <p>你好啊!!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
--- pages/test.js
+++ pages/test.js
@@ -1,8 +1,8 @@
 import Market_aside from '@/components/Leo/market_aside'
-import Market_aside_button from '@/components/Leo/market_aside_button'
 import MarketCard from '@/components/Leo/market_card'
 import MemberBreadcrumbs from '@/components/Leo/member_breadcrumbs'
 import Member_info from '@/components/Leo/member_info'
+import Playground from '@/components/Leo/market_playground'
 import React from 'react'
 
 export default function test() {
@@ -10,9 +10,9 @@ export default function test() {
     <div>
       <Member_info />
       <MemberBreadcrumbs />
-      <Market_aside_button />
       <Market_aside />
       <MarketCard />
+      <Playground />
     </div>
   )
 }
