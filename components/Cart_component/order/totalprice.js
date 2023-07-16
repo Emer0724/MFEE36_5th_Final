c33580bacd59355e74bd69f169142bdc557c3ad7
tommy <tonlien89@yahoo.com.tw>
2023-07-16 15:41:22 +0800

    0716 0341

--- components/Cart_component/order/totalprice.js
+++ components/Cart_component/order/totalprice.js
@@ -1,14 +1,28 @@
 import DeepButton from '@/components/common/CBtn/DeepgreenBtn'
 import React from 'react'
+import { useState,useEffect } from 'react'
 
 export default function OrderTotalPrice({btncontent,border1}) {
+  const [windowWidth ,setWindowWidth] = useState(null)
+   useEffect(()=>{
+    const ResizeHandle=()=>{
+      setWindowWidth({
+        width:innerWidth
+      })
+    }
+    window.addEventListener('resize',ResizeHandle);
+    return()=>{
+      window.removeEventListener('resize',ResizeHandle)
+    }
+   },[])
+
   const style1 = {
     display:"flex",
     flexDirection: "column",
     justifyContent:"space-around",
     alignItems:"center",
     borderRadius:"8px",
-    width:"500px",
+    width: windowWidth>500?"500px":"100%",
     height:"550px",
     padding:"20px",
     border:border1,
@@ -25,7 +39,7 @@ export default function OrderTotalPrice({btncontent,border1}) {
   const subprice =["商品金額","貨運費用","折價卷","總金額"]
   const price =[5000,"+60","-100",4960]
   return (
-    <div style={style1}>
+    <div style={style1} >
         <div><h1>訂單金額</h1></div>
           {subprice.map((v,i)=>{
               const v2 = price[i]
--- pages/order/index.js
+++ pages/order/index.js
@@ -2,21 +2,16 @@ import CartTitle from '@/components/Cart_component/Cart_title'
 import OrderForm from '@/components/Cart_component/order/order_form'
 import OrderState from '@/components/Cart_component/order/order_state'
 import OrderTotalPrice from '@/components/Cart_component/order/totalprice'
-import NavBar1 from '@/components/common/navbar/NavBar'
 import React from 'react'
+import styles from '@/pages/order/Orderindex.module.css'
 
-const checkoutcontain = {
-  display:"flex",
-  justifyContent:"space-around",
-  alignItems:"center"
-}
 
 export default function checkout() {
   return (
     <>
         <OrderState/>
         <CartTitle titlecontent={"選擇喜歡的寄送方式和付款方式吧"}/>
-        <div style={checkoutcontain}>
+        <div className={styles.checkoutcontain}>
           <OrderForm/>
           <OrderTotalPrice btncontent={"下一步，確認商品"} border1={"1px solid #52796F"}/>
         </div>
