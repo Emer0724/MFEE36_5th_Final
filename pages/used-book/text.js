9449a578b0349dacadc96f5e6d8a2aebdb88f7e4
leeting <leeting110213@gmail.com>
2023-07-17 16:00:24 +0800

    0714 16:00

--- pages/used-book/text.js
+++ pages/used-book/text.js
@@ -2,23 +2,27 @@ import React from 'react'
 import UsedList from '@/components/used/used-list/index'
 import { useState } from 'react'
 import UsedUpCheck from '@/components/used/used-upcheck'
-
+import PopUp from '@/components/common/popup/popup'
 
 export default function Text() {
-    const datas = [
-        { book_state: '近全新', price: '198', inventory: '10' },
-        { book_state: '良好', price: '198', inventory: '10' },
-        { book_state: '不良', price: '198', inventory: '10' },
-      ]
-    // const datas=['noUsedBook']
-    const ISBN='9876445237'
-     
+  const datas = [
+    { book_state: '近全新', price: '198', inventory: '10' },
+    { book_state: '良好', price: '198', inventory: '10' },
+    { book_state: '不良', price: '198', inventory: '10' },
+  ]
+  // const datas=['noUsedBook']
+  const ISBN = '9876445237'
+  const getsome = () => {
+    return 'getsome'
+  }
+
   return (
-   <>
-   <div className='w-50'>
-   <UsedList datas={datas} ISBN={ISBN}  ></UsedList>
-   </div>
-   <UsedUpCheck/>
-   </>
+    <>
+      <div className="w-50">
+        <UsedList datas={datas} ISBN={ISBN}></UsedList>
+      </div>
+      <UsedUpCheck />
+      {/* <PopUp content1={12346} content2={'5454646'} onclick1={getsome} /> */}
+    </>
   )
 }
