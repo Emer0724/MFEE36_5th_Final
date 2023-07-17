7e13665eec4cd12400d8eaafdbaaf332a6a15974
tommy <tonlien89@yahoo.com.tw>
2023-07-17 12:48:05 +0800

    07171200

--- components/Cart_component/order/productcheck.js
+++ components/Cart_component/order/productcheck.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import fake from "@/assets/Cart_Image/fake.svg"
+import styles from "@/components/Cart_component/order/productcheck.module.css"
 
 export default function ProductCheck() {
 
@@ -34,15 +35,16 @@ export default function ProductCheck() {
       }
     ]
   return (
-    <div>
+    <div className={styles.checkcontain}>
         {product.map((v,i)=>{
             return(
-            <div key={i}>
-                <div><Image src={fake} alt='icon'/></div>
-                <div>
-                    <p>{v.Productname}</p>
-                    <p>{v.ISBN}</p>
-                    <p>{v.price*v.count}</p>
+            <div key={i} className={styles.checkrow}>
+                <div className={styles.checklist}>
+                    <p className={styles.checkname}><Image src={fake} alt='icon'/>{v.Productname}</p>
+                    <p className={styles.checkisbn}>{v.ISBN}</p>
+                    <p className={styles.checkprice}>${v.price}</p>
+                    <p className={styles.checkcount}>{v.count}</p>
+                    <p className={styles.checktotal}>${v.price*v.count}</p>
                 </div>
             </div>
             )
--- pages/order/productcheck.js
+++ pages/order/productcheck.js
@@ -1,6 +1,6 @@
 import CartTitle from '@/components/Cart_component/Cart_title'
-import CurtProduct from '@/components/Cart_component/cart/Cart_ProductList'
 import OrderState from '@/components/Cart_component/order/order_state'
+import ProductCheck from '@/components/Cart_component/order/productcheck'
 import OrderTotalPrice from '@/components/Cart_component/order/totalprice'
 import React from 'react'
 
@@ -15,7 +15,7 @@ export default function productcheck() {
     <div>
         <OrderState/>
         <CartTitle titlecontent={"再次確認商品後，前往付款吧"}/>
-        <CurtProduct display1={'none'}/>
+        <ProductCheck/>
         <div style={style1}>
           <OrderTotalPrice btncontent={"下一步，前往付款"}/>
         </div>
