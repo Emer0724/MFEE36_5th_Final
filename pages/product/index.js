bf1dc876d6665d8a3643d13396f64e2478332e7a
Leo Hu <innocence88226@gmail.com>
2023-07-25 18:17:19 +0800

    leo 0725 1817

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import Playground from '@/components/Leo/market_playground'
 import Aside from '@/components/Leo/market_aside'
 import Wish from '@/components/Leo/market_wish'
 import CarouselComponent from '@/components/Leo/carousel'
 
-export default function product() {
+export default function Product() {
   return (
     <>
       <CarouselComponent />
