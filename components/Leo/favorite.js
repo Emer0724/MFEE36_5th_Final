16d1f25725e2d2cc69c866af537376136e6d27f5
Leo Hu <innocence88226@gmail.com>
2023-08-02 18:23:58 +0800

    Leo 0802 1822 幹你娘追蹤功能花了我6個半小時還沒完成版 (差取消追蹤)

--- components/Leo/favorite.js
+++ components/Leo/favorite.js
@@ -6,14 +6,26 @@ export default function Favorite(result) {
   // const member_id =
   const user_info = JSON.parse(localStorage.getItem('auth')) //localStorage.getItem('auth') 取得的是字串 先轉成JSON
   const { id } = user_info
-  // console.log(user_info)
-  // console.log(id)
-  // console.log('八嘎nono')
+  // const member_id = id.toString()
   const [mark, setMark] = useState(false)
 
   const checkMark = () => {
-    const isBookmarkedFromBackend = false
-    setMark(isBookmarkedFromBackend)
+    fetch(`${process.env.API_SERVER}/market/recommand`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ISBN: ISBN, member_id: id }),
+    })
+      .then((res) => res.json())
+      .then((datas) => {
+        console.log('後端回傳結果:', datas)
+        // 根據收藏資料來更新按鈕的狀態
+        setMark(datas.rows.length > 0)
+      })
+      .catch((error) => {
+        console.error('請求發送錯誤', error)
+      })
   }
   useEffect(() => {
     checkMark()
@@ -39,6 +51,12 @@ export default function Favorite(result) {
         .then((datas) => {
           console.log('後端回傳結果:', datas)
           checkMark()
+          console.log(datas.rows.length)
+          if (datas.rows.length > 0) {
+            setMark(true)
+          } else {
+            setMark(false)
+          }
         })
         .catch((error) => {
           console.error('請求發送錯誤', error)
@@ -52,19 +70,12 @@ export default function Favorite(result) {
       }
     }
   }
-
   return (
     <>
       <button
-        className={`${styles.fbtn} ${styles.i}`}
+        className={`${styles.fbtn}  ${mark ? styles.true : styles.false}`}
         onClick={() => recommand()}
       ></button>
     </>
   )
 }
-
-// className={`${
-//   router.asPath === '/dashboard/coupon'
-//     ? styles.coupon
-//     : styles.coupon_ex
-// }`}
