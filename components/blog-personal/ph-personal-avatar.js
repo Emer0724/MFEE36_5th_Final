e6ab0c598fa31bc67911b638ed4118c3b7866e6d
leeting <leeting110213@gmail.com>
2023-08-13 13:37:25 +0800

    0813 0137

--- components/blog-personal/ph-personal-avatar.js
+++ components/blog-personal/ph-personal-avatar.js
@@ -3,20 +3,22 @@ import Image from 'next/image'
 import img1 from '@/public/blog-img/images.jpg'
 
 export default function PhPersonalAvatar() {
-    return (
-        <>
-            <div className={`${stlye.phava}`}>
-                <div className={`${stlye.chenpersonal} d-flex`}>
-                    <div>
-                        <div>
-                            <Image src={img1} className={stlye.chenheadimg}/>
-                        </div>
-                    </div>
-                    <div className={`text-decoration-none`}>
-                            <div className={`${stlye.chenwd} fw-bold mb-0 mt-1 text-dark pb-2`}>阿巴</div>
-                        </div>
-                </div>
-            </div>       
-        </>
-    )
+  return (
+    <>
+      <div className={`${stlye.phava}`}>
+        <div className={`${stlye.chenpersonal} d-flex`}>
+          <div>
+            <div>
+              <Image src={img1} className={stlye.chenheadimg} />
+            </div>
+          </div>
+          <div className={`text-decoration-none`}>
+            <div className={`${stlye.chenwd} fw-bold mb-0 mt-1 text-dark pb-2`}>
+              阿巴
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
